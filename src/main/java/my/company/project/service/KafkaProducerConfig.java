07e4bdf42deaad1b531a08ea650aa8d827cef959
Venafra Raffaele <raffaele.venafra@axerve.com>
2021-03-30 19:58:53 +0200

    Removed unused classes

---
+++ src/main/java/my/company/project/service/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package my.company.project.service;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+public class KafkaProducerConfig {
+
+    KafkaProperties kafkaProperties;
+
+    @Autowired
+    public KafkaProducerConfig(
+            KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        Map<String, Object> configs = kafkaProperties.buildProducerProperties();
+        ProducerFactory<String, Object> defaultKafkaProducerFactory = new DefaultKafkaProducerFactory<>(configs);
+        return new KafkaTemplate<>(defaultKafkaProducerFactory);
+    }
+}
